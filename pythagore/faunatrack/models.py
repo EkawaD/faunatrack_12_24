c9e4fad56dd93637511d64db3bab5a6d26a54c1d
Bastien <bastien@pythagore.com>
2024-12-16 12:33:41 +0100

    feat: first endpoint and migrations

---
+++ pythagore/faunatrack/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# class Guitare(models.Model):
+#     marque = models.CharField(max_length=255, default="", verbose_name="marque_guitare")
+#     nom = models.CharField(max_length=255, default="")
---
+++ pythagore/poo.py
@@ -0,0 +1,72 @@
+def func1() -> str:
+    return "Qqchose"
+
+def func1() -> str:
+    return "Qqchose"
+
+def func1() -> str:
+    return "Qqchose"
+
+ 
+
+class A():
+    attr1 = 1
+    attr2 = "cequejeveux"
+
+    def func1():
+        pass
+
+class B():
+    def func2():
+        pass
+
+class Moteur:
+    puissance = ??? 
+
+
+
+class Voiture :
+
+    structure = ???
+    moteur: Moteur = Moteur1
+    couleur 
+    nb_passagers 
+    charge = 200kg
+
+    def rouler(carburant):
+        return ??
+
+    def tourner()
+
+    def park()
+
+    def unpark()
+
+
+v = Voiture()
+v.unpark()
+v.drive()
+pos_inital = v.position
+while v.positon != pos_initial:
+    v.tourner('droite')
+
+
+# Composition
+class Parking():
+    voiture_list: list[Voiture] = []
+
+# Héritage
+class Camion(Voiture):
+    nb_passagers = 2
+    charge utile = 2T
+    
+    def rouler():
+        pass
+
+
+c= Camion()
+c.rouler()
+
+class Autoroute(Route):
+
+    list_voiture = []
